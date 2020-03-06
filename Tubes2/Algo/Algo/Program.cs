38fbb9b7bfef1affcc745415eaf2a8e46ee1a8a8
Kamal S <muha.kamal@gmail.com>
2020-03-07 01:38:44 +0700

    Little bit more

--- Tubes2/Algo/Algo/Program.cs
+++ Tubes2/Algo/Algo/Program.cs
@@ -11,6 +11,18 @@ namespace Algo
         const Double EPS = 1e-8;
 
         static int[] populasi;
+        static Dictionary<string, int> nameIndex;
+        static List<(int, Double)>[] adjList;
+        static int TAkhir;
+        static int[] TAwal;
+
+
+        static int root;
+        static int n;
+        static string[] getNameFromidx;
+        static List<int> infectedCity;
+        static List<(int, int)> edgeList;
+        static List<(int, int)> infectedEdge;
 
         static Double Ifunc(int a, int tt)
         { // kota, hari total a terinfeksi
@@ -24,32 +36,32 @@ namespace Algo
             return Ifunc(a, tt) * fac;
         }
 
-        static void Main(string[] args)
-        {
+        static void input()
+        {   // input dari file populasi.txt dan sisi.txt
             string[] lines = System.IO.File.ReadAllLines("../../../populasi.txt");
             string[] line = lines[0].Split(' ');
 
-            int n = Int32.Parse(line[0]);
+            n = Int32.Parse(line[0]);
             string awal = line[1];
 
-            populasi = new int[n + 2];
-            string[] getNameFromidx = new string[n + 2];
-            var nameIndex = new Dictionary<string, int>();
-            
+            populasi = new int[n + 1];
+            getNameFromidx = new string[n + 1];
+            nameIndex = new Dictionary<string, int>();
+
             for (int i = 1; i <= n; i++)
             {
                 line = lines[i].Split(' ');
                 getNameFromidx[i] = line[0];
                 populasi[i] = Int32.Parse(line[1]);
                 nameIndex[getNameFromidx[i]] = i;
-                //Console.WriteLine(populasi[i]);
             }
 
             lines = System.IO.File.ReadAllLines("../../../sisi.txt");
             int root = nameIndex[awal];
             line = lines[0].Split();
             int m = Int32.Parse(line[0]);
-            List<(int, Double)>[] adjList = new List<(int, Double)>[m + 1];
+            adjList = new List<(int, Double)>[m + 1];
+            edgeList = new List<(int, int)>();
 
             // read adjancency list
             for (int i = 1; i <= m; i++)
@@ -63,20 +75,23 @@ namespace Algo
                     adjList[u] = new List<(int, Double)>();
                 }
                 adjList[u].Add((v, w));
+                edgeList.Add((u, v));
             }
-
-            line = lines[m + 1].Split(' ');
-            int TAkhir = Int32.Parse(line[0]);
-            int[] tawal = new int[n + 2];
+            TAwal = new int[n + 2];
             for (int i = 1; i <= n; i++)
             {
-                tawal[i] = INF;
+                TAwal[i] = INF;
             }
-            tawal[root] = 0;
+            TAwal[root] = 0;
+        }
 
+        static void BFS(int T)
+        {   // parameter T = hari
+            TAkhir = T;
             var prioQueue = new SortedSet<(int, int)>();
-            for (int i = 1; i <= n; i++) {
-                (int, int) x = (tawal[i], i);
+            for (int i = 1; i <= n; i++)
+            {
+                (int, int) x = (TAwal[i], i);
                 prioQueue.Add(x);
             }
 
@@ -84,16 +99,16 @@ namespace Algo
             {
                 var cur = prioQueue.First().Item2;
                 prioQueue.Remove(prioQueue.First());
-                if (tawal[cur] == INF) break;
+                if (TAwal[cur] == INF) break;
 
                 foreach (var dest in adjList[cur])
                 {
-                    if (Sfunc(cur, dest.Item2, TAkhir - tawal[cur]) >= 1.0 - EPS)
+                    if (Sfunc(cur, dest.Item2, TAkhir - TAwal[cur]) >= 1.0 - EPS)
                     {
-                        int low = 0, high = TAkhir - tawal[cur];
+                        int low = 0, high = TAkhir - TAwal[cur];
 
                         while (low < high)
-                        {
+                        {   // binary search untuk mencari durasi yang dibutuhkan untuk menginfeksi kota lain
                             int mid = (low + high) / 2;
                             if (Sfunc(cur, dest.Item2, mid) >= 1.0 - EPS)
                             {
@@ -105,26 +120,74 @@ namespace Algo
                             }
                         }
 
-                        int new_t = high + tawal[cur];
-                        if (new_t < tawal[dest.Item1])
+                        int new_t = high + TAwal[cur];
+                        if (new_t < TAwal[dest.Item1])
                         {
-                            (int, int) temp = (tawal[dest.Item1], dest.Item1);
+                            (int, int) temp = (TAwal[dest.Item1], dest.Item1);
                             prioQueue.Remove(temp);
-                            tawal[dest.Item1] = new_t;
-                            temp = (tawal[dest.Item1], dest.Item1);
+                            TAwal[dest.Item1] = new_t;
+                            temp = (TAwal[dest.Item1], dest.Item1);
                             prioQueue.Add(temp);
                         }
                     }
                 }
             }
 
-            Console.WriteLine("Kota yang terinfeksi: ");
+            infectedCity = new List<int>();
             for (int i = 1; i <= n; i++)
-            {
-                if (tawal[i] != INF)
+            {   // mencatat semua kota yang berwarna merah (terinfeksi)
+                if (TAwal[i] != INF)
                 {
-                    Console.WriteLine(getNameFromidx[i]);
+                    infectedCity.Add(i);
                 }
+            }
+
+            infectedEdge = new List<(int, int)>();
+            foreach (int node in infectedCity)
+            {
+                foreach (var dest in adjList[node])
+                {   // mencatat semua edge yang berwarna merah (S(A, B) >= 1)
+                    if (Sfunc(node, dest.Item2, TAkhir - TAwal[node]) >= 1.0 - EPS)
+                    {
+                        infectedEdge.Add((node, dest.Item1));
+                    }
+                }
+            }
+        }
+
+        public static int getCityCount()
+        {
+            return n;
+        }
+
+        public static string[] allCityName()
+        {
+            return getNameFromidx;
+        }
+
+        public static List<int> getInfectedCity()
+        {
+            return infectedCity;
+        }
+
+        public static List<(int, int)> getEdgeList()
+        {
+            return edgeList;
+        }
+
+        public static List<(int, int)> getInfectedEdge()
+        {
+            return infectedEdge;
+        }
+
+        static void Main(string[] args)
+        {
+            input();  // input dari file populasi.txt dan sisi.txt
+            BFS(30);  // T = 30 hari
+            Console.WriteLine("Kota yang terinfeksi: ");
+            foreach (int x in infectedCity)
+            {
+                Console.WriteLine(getNameFromidx[x]);
             }
         }
     }
